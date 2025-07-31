21cf6bf9ac238d5d051879fe624358004963a079
pujitha-2005-art <pujithadasari26@gmail.com>
2025-07-31 11:41:41 +0530

    Create if-elif condition.py

---
+++ if-elif condition.py
@@ -0,0 +1,8 @@
+    "a=int(input('enter the number'))\n",
+    "b=int(input('enter the number'))\n",
+    "if a>b:\n",
+    "    print('largest',a)\n",
+    "elif a<b:\n",
+    "    print('largest',b)\n",
+    "else:\n",
+    "    print('both are equal')"
